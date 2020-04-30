a5e830b936fb57b91485137a927dbe0174d691b4
Christoper Tracey <ctracey@paconserve.org>
2020-04-30 10:16:45 -0400

    begins of date processing

--- scripts_new/timelapse_hydro.r
+++ scripts_new/timelapse_hydro.r
@@ -1,14 +1,31 @@
 library(tidyverse)
 library(here)
 library(ggplot2)
+library(lubridate)
 
 
 
+
+# get local sunrise/sunset time
+
+#######################################################################################################################
 # beginnings of a function to read exif information from the camera
 library(exifr)
 files <- list.files(here::here("data_new","sites","testsite1"), recursive=TRUE, pattern="JPG", full.names=TRUE)
 exifinfo <- read_exif(files)
 
+# find the photos taken at 15minute intervals
+phototimes <- exifinfo$FileModifyDate
+phototimes
+datetime <- ymd_hms(phototimes) # convert to date format
+datetime <- with_tz(datetime, "America/New_York") # convert to eastern time
+
+
+datetime1 <- datetime[which(minute(datetime) %in% c(0,15,30,45))]
+
+
+
+######################################################################################################################
 # function to get gage data
 library(dataRetrieval)
 siteNumber <- "03081500"
@@ -20,3 +37,6 @@ dischargeUnit <- readNWISuv(siteNumber, parameterCd, startDate, endDate)
 dischargeUnit <- renameNWISColumns(dischargeUnit)
 
 plot(dischargeUnit$dateTime,dischargeUnit$GH_Inst)
+
+
+
